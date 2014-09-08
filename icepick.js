d2092fb66a6ed89646c2df989b94974356d77e48
Alexander Early <aearly@fluid.com>
2014-09-08 16:53:44 -0700

    obj -> coll for first arg

--- icepick.js
+++ icepick.js
@@ -42,50 +42,50 @@ function objClone(obj) {
   return result;
 }
 
-function clone(obj) {
-  if (Array.isArray(obj)) {
-    return arrayClone(obj);
+function clone(coll) {
+  if (Array.isArray(coll)) {
+    return arrayClone(coll);
   } else {
-    return objClone(obj);
+    return objClone(coll);
   }
 }
 
-function baseFreeze(obj, prevNodes) {
-  if (prevNodes.filter(function (node) { return node === obj; }).length > 0) {
+function baseFreeze(coll, prevNodes) {
+  if (prevNodes.filter(function (node) { return node === coll; }).length > 0) {
     throw new Error("object has a reference cycle");
   }
 
-  Object.freeze(obj);
-  prevNodes.push(obj);
-  Object.keys(obj).forEach(function (key) {
-    var prop = obj[key];
+  Object.freeze(coll);
+  prevNodes.push(coll);
+  Object.keys(coll).forEach(function (key) {
+    var prop = coll[key];
     if (weCareAbout(prop)) {
       baseFreeze(prop, prevNodes);
     }
   });
   prevNodes.pop();
 
-  return obj;
+  return coll;
 }
 
 /**
  * recrursively freeze an object and all its child objects
- * @param  {Object|Array} obj
+ * @param  {Object|Array} coll
  * @return {Object|Array}
  */
-exports.freeze = function freeze(obj) {
-  return baseFreeze(obj, []);
+exports.freeze = function freeze(coll) {
+  return baseFreeze(coll, []);
 };
 
 /**
  * set a value on an object or array
- * @param  {Object|Array}  obj
+ * @param  {Object|Array}  coll
  * @param  {String|Number} key   Key or index
  * @param  {Object}        value
  * @return {Object|Array}        new object hierarchy with modifications
  */
-exports.assoc = function assoc(obj, key, value) {
-  var newObj = clone(obj);
+exports.assoc = function assoc(coll, key, value) {
+  var newObj = clone(coll);
 
   if (weCareAbout(value) && !Object.isFrozen(value)) {
     value = baseFreeze(value, []);
@@ -98,46 +98,46 @@ exports.assoc = function assoc(obj, key, value) {
 
 /**
  * set a value deep in a hierarchical structure
- * @param  {Object|Array} obj
+ * @param  {Object|Array} coll
  * @param  {Array}        path    A list of keys to traverse
  * @param  {Object}       value
  * @return {Object|Array}       new object hierarchy with modifications
  */
-exports.assocIn = function assocIn(obj, path, value) {
+exports.assocIn = function assocIn(coll, path, value) {
   var key0 = path[0];
   if (path.length === 1) {
     // simplest case is a 1-element array.  Just a simple assoc.
-    return i.assoc(obj, key0, value);
+    return i.assoc(coll, key0, value);
   } else {
     // break the problem down.  Assoc this object with the first key
     // and the result of assocIn with the rest of the keys
-    return i.assoc(obj, key0, assocIn(obj[key0], path.slice(1), value));
+    return i.assoc(coll, key0, assocIn(coll[key0], path.slice(1), value));
   }
 };
 
 /**
  * get an object from a hierachy based on an array of keys
- * @param  {Object|Array} obj
+ * @param  {Object|Array} coll
  * @param  {Array}        path    list of keys
  * @return {Object}       value, or undefined
  */
-function baseGet(obj, path) {
+function baseGet(coll, path) {
   return (path || []).reduce(function (val, key) {
     return val[key];
-  }, obj);
+  }, coll);
 }
 
 exports.getIn = baseGet;
 
 /**
  * Update a value in a hierarchy
- * @param  {Object|Array}   obj
+ * @param  {Object|Array}   coll
  * @param  {Array}          path     list of keys
  * @param  {Function} callback The existing value with be passed to this.
  *                             Return the new value to set
  * @return {Object|Array}      new object hierarchy with modifications
  */
-exports.updateIn = function updateIn(obj, path, callback) {
-  var existingVal = baseGet(obj, path);
-  return i.assocIn(obj, path, callback(existingVal));
+exports.updateIn = function updateIn(coll, path, callback) {
+  var existingVal = baseGet(coll, path);
+  return i.assocIn(coll, path, callback(existingVal));
 };
